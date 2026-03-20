6a46f5288e08032373ee7233b9b6a3736bfe3fe8
HanLouise <rysharprules@gmail.com>
2026-03-20 12:00:44 +0000

    ch1 question review

---
+++ src/rysharp/chapter/_01/Question07.java
@@ -0,0 +1,13 @@
+package rysharp.chapter._01;
+
+public class Question07 {
+    private int numForks;
+
+    public static void main(String... args) {
+        int numKnives;
+        System.out.print("""
+                "# forks = " + numForks +
+                 " # knives = " + numKnives +
+                # cups = 0""");
+    }
+}
---
+++ src/rysharp/chapter/_01/Question16.java
@@ -0,0 +1,12 @@
+package rysharp.chapter._01;
+
+public class Question16 {
+
+    public static void main(String... args) {
+        var blocky = """
+   squirrel \s
+   pigeon   \
+   termite""";
+        System.out.print(blocky);
+    }
+}
